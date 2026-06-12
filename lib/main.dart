8319f6d64bf539cfd1c8e511a67737b3f7cd2322
SOHAM <sohamindurkar12@gmail.com>
2026-06-12 22:20:40 +0530

    glicth fixed

--- lib/main.dart
+++ lib/main.dart
@@ -102,10 +102,10 @@ class _SmartBillingAppState extends State<SmartBillingApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       builder: (context, child) {
+        final data = MediaQuery.maybeOf(context);
+        if (data == null) return child!;
         return MediaQuery(
-          data: MediaQuery.of(
-            context,
-          ).copyWith(textScaler: const TextScaler.linear(1.0)),
+          data: data.copyWith(textScaler: const TextScaler.linear(1.0)),
           child: child!,
         );
       },
